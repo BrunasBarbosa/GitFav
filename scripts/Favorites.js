f90149b6421546896e486ae523edd3765a71b4c0
Bruna Barbosa <brunasbarbosa951@gmail.com>
2022-09-17 16:27:46 -0300

    create search button hover

--- scripts/Favorites.js
+++ scripts/Favorites.js
@@ -14,7 +14,7 @@ export class GithubUser {
 }
 export class Favorites {
   constructor(root) {
-    this.root = document.querySelector(root)
+    this.root = document.querySelector(root);
     this.load();
   }
 
@@ -31,29 +31,29 @@ export class Favorites {
       const userExist = this.entries.find(entry => entry.login === username)
 
       if (userExist) {
-        throw new Error('Usuário já cadastrado.')
+        throw new Error('Usuário já cadastrado.');
       }
 
-      const user = await GithubUser.search(username)
+      const user = await GithubUser.search(username);
 
       if (user.login === undefined) {
-        throw new Error('Usuário não encontrado.')
+        throw new Error('Usuário não encontrado.');
       }
 
       this.entries = [user, ...this.entries]
-      this.update()
-      this.save()
+      this.update();
+      this.save();
     } catch (error) {
-      alert(error.message)
+      alert(error.message);
     }
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
 
-    this.entries = filteredEntries
-    this.update()
-    this.save()
+    this.entries = filteredEntries;
+    this.update();
+    this.save();
   }
 }
 
@@ -71,6 +71,11 @@ export class FavoritesView extends Favorites {
 
   onadd() {
     const addButton = this.root.querySelector('.search button');
+    const star = this.root.querySelector('.star path');
+
+    addButton.addEventListener('mouseover', () => star.style.fill = 'var(--button-search-color)');
+
+    addButton.addEventListener('mouseout', () => star.style.fill = 'white');
 
     addButton.onclick = () => {
       const { value } = this.root.querySelector('.search input')
